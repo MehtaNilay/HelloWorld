86b445f9e954bed4e4fc9f1099e6ef9778e0a3e7
10056x <10056x>
2016-09-10 01:02:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a10056x/helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a10056x.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
